941db0fc856ab9be9dc03a91ec01b69c420c13f1
김명진 <isitreal161.1@gmail.com>
2025-07-14 20:15:44 +0900

    add job, rank enum

--- src/main/java/com/example/demo/domain/rank/RankRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.domain.rank;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RankRepository extends JpaRepository<Rank, Long> {
-    // 추가적인 쿼리 메서드는 필요시 선언
-} 
