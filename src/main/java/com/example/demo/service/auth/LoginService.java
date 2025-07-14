d2b14f5942a2ba06ccc4a9a71efc0be42c14c2d5
박지민 <pjmlove2008@gmail.com>
2025-07-15 01:41:01 +0900

    feat: LoginService 구현

--- src/main/java/com/example/demo/service/auth/LoginService.java
+++ src/main/java/com/example/demo/service/auth/LoginService.java
@@ -1,7 +1,43 @@
 package com.example.demo.service.auth;
 
+import com.example.demo.domain.auth.exception.PasswordIncorrectException;
+import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.exception.UserNotFoundException;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.global.security.jwt.JwtProperties;
+import com.example.demo.global.security.jwt.JwtTokenProvider;
+import com.example.demo.presentation.auth.dto.request.LoginRequest;
+import com.example.demo.presentation.auth.dto.response.TokenResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @Service
+@RequiredArgsConstructor
 public class LoginService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProperties jwtProperties;
+    private final UserRepository userRepository;
+
+    public TokenResponse login(LoginRequest request) {
+        User user = userRepository.findByAccountId(request.getAccountId())
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw PasswordIncorrectException.EXCEPTION;
+        }
+
+        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Seoul"));
+        return TokenResponse.builder()
+                .accessToken(jwtTokenProvider.generateAccessToken(request.getAccountId()))
+                .accessTokenExpiresAt(now.plusSeconds(jwtProperties.getAccessExp()))
+                .refreshToken(jwtTokenProvider.generateRefreshToken(request.getAccountId()))
+                .refreshTokenExpiresAt(now.plusSeconds(jwtProperties.getRefreshExp()))
+                .build();
+    }
 }
