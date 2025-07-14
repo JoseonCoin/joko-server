281411ae95d6f898d84f83a1208484a0963e174d
박지민 <pjmlove2008@gmail.com>
2025-07-15 01:40:28 +0900

    feat: SignupService 구현

--- src/main/java/com/example/demo/service/auth/SignupService.java
+++ src/main/java/com/example/demo/service/auth/SignupService.java
@@ -1,8 +1,33 @@
 package com.example.demo.service.auth;
 
+import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.exception.UserAlreadyExistsException;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.global.security.jwt.JwtProperties;
+import com.example.demo.global.security.jwt.JwtTokenProvider;
+import com.example.demo.presentation.auth.dto.request.SignupRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SignupService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void signup(SignupRequest request) {
+        if (userRepository.findByAccountId(request.accountId()).isPresent()) {
+            throw UserAlreadyExistsException.EXCEPTION;
+        }
+
+        User user = User.builder()
+                .accountId(request.accountId())
+                .username(request.username())
+                .password(passwordEncoder.encode(request.password()))
+                .build();
+
+        userRepository.save(user);
+    }
 }
