705ac79a86a7b7d81c14a8de0d5f2edb18caeb97
박지민 <pjmlove2008@gmail.com>
2025-07-15 15:22:10 +0900

    feat: QuizResponse 구현

---
+++ src/main/java/com/example/demo/presentation/quiz/dto/response/QuizResponse.java
@@ -0,0 +1,15 @@
+package com.example.demo.presentation.quiz.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QuizResponse {
+
+    private Long quizId;
+    private String question;
+    private List<String> options;
+}
