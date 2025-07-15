fa0590d689b7cc3a04804cc2a99a7df5559bfcfd
박지민 <pjmlove2008@gmail.com>
2025-07-15 15:20:35 +0900

    feat: QuizController 구현

---
+++ src/main/java/com/example/demo/presentation/quiz/QuizController.java
@@ -0,0 +1,49 @@
+package com.example.demo.presentation.quiz;
+
+import com.example.demo.domain.quiz.Quiz;
+import com.example.demo.domain.user.User;
+import com.example.demo.domain.user.repository.UserRepository;
+import com.example.demo.presentation.quiz.dto.request.QuizSubmitRequest;
+import com.example.demo.presentation.quiz.dto.response.QuizResponse;
+import com.example.demo.presentation.quiz.dto.response.QuizResultResponse;
+import com.example.demo.service.quiz.QuizRetrievalService;
+import com.example.demo.service.quiz.QuizSubmissionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/quiz")
+@RequiredArgsConstructor
+public class QuizController {
+    private final QuizSubmissionService quizSubmissionService;
+    private final QuizRetrievalService quizRetrievalService;
+    private final UserRepository userRepository;
+
+    @PostMapping("/submit")
+    public QuizResultResponse submitQuiz(@RequestParam Long id, @RequestBody QuizSubmitRequest request) {
+        return quizSubmissionService.submitQuiz(id, request);
+    }
+
+    @GetMapping("/{quizId}")
+    public QuizResponse getQuiz(@PathVariable Long quizId) {
+        return quizRetrievalService.getQuizById(quizId);
+    }
+
+    @GetMapping("/random")
+    public QuizResponse getRandomQuiz(@AuthenticationPrincipal UserDetails userDetails) {
+        String accountId = userDetails.getUsername();
+        User user = userRepository.findByAccountId(accountId)
+                .orElseThrow(() -> new UsernameNotFoundException(accountId));
+        return quizRetrievalService.getRandomQuizByUserId(user.getId());
+    }
+
+    @GetMapping("/list")
+    public List<Quiz> getQuizList(){
+        return quizRetrievalService.getQuizList();
+    }
+}
