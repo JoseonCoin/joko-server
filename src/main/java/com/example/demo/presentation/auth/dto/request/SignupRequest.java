bbfac16bdbd777246afd1e0067abf1377fd7b32f
박지민 <pjmlove2008@gmail.com>
2025-07-15 15:20:07 +0900

    feat: SignupRequest에 rank 추가

--- src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
@@ -1,7 +1,9 @@
 package com.example.demo.presentation.auth.dto.request;
 
+import com.example.demo.domain.rank.Rank;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 
 public record SignupRequest (
 
@@ -13,5 +15,7 @@ public record SignupRequest (
 
         @NotBlank(message = "비밀번호는 필수입니다.")
         @Size(min = 8, max = 255, message = "비밀번호는 8자 이상 입력해야 합니다.")
-        String password
+        String password,
+
+        Rank rank
 ){}
