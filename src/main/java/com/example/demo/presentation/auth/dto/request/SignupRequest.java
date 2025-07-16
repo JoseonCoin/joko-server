a5b3bad2e2a2314b3c157a7a68565ff7bfa0518a
박지민 <xavexxn@gmail.com>
2025-07-16 19:55:10 +0900

    nice

--- src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
@@ -1,16 +1,11 @@
 package com.example.demo.presentation.auth.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 
 public record SignupRequest (
 
-        @NotBlank(message = "사용자 이름은 필수입니다.")
         String username,
 
-        @NotBlank(message = "accountId는 필수입니다.")
         String accountId,
 
-        @NotBlank(message = "비밀번호는 필수입니다.")
         String password
 ){}
