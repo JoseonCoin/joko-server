65fd60f727d6d476c8f93440facd9767f9e59c08
박지민 <pjmlove2008@gmail.com>
2025-07-16 19:25:33 +0900

    chore : securityConfig update

--- src/main/java/com/example/demo/global/security/SecurityConfig.java
+++ src/main/java/com/example/demo/global/security/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(configurer -> configurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorizeRequests -> authorizeRequests
-                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/auth/signup").permitAll()
                         .requestMatchers("/quiz/**").authenticated()
                         .requestMatchers("/item/**").authenticated()
                         .requestMatchers("/user/**").authenticated()
