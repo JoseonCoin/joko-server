016b3a521a8d003e573ecd2df76eda8484fac6c9
박지민 <pjmlove2008@gmail.com>
2025-07-16 19:30:06 +0900

    chore : 제약조건 삭제

--- src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
+++ src/main/java/com/example/demo/presentation/auth/dto/request/SignupRequest.java
@@ -6,13 +6,11 @@ import jakarta.validation.constraints.Size;
 public record SignupRequest (
 
         @NotBlank(message = "사용자 이름은 필수입니다.")
-        @Size(max = 8)
         String username,
 
         @NotBlank(message = "accountId는 필수입니다.")
         String accountId,
 
         @NotBlank(message = "비밀번호는 필수입니다.")
-        @Size(min = 8, max = 255, message = "비밀번호는 8자 이상 입력해야 합니다.")
         String password
 ){}
